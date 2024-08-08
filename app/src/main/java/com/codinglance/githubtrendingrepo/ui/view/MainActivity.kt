6f39c4821b0ab66d9ee3c92f20d3200331253915
mohdaasim <mohdaasim71@gmail.com>
2024-08-08 17:05:38 +0530

    Add Battery Manager to get battery information

--- app/src/main/java/com/codinglance/githubtrendingrepo/ui/view/MainActivity.kt
+++ app/src/main/java/com/codinglance/githubtrendingrepo/ui/view/MainActivity.kt
@@ -1,28 +1,42 @@
 package com.codinglance.githubtrendingrepo.ui.view
 
+import android.annotation.SuppressLint
 import android.app.ProgressDialog
+import android.content.Context
+import android.os.BatteryManager
+import android.os.Build
 import android.os.Bundle
-import android.os.SystemClock
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.codinglance.githubtrendingrepo.BaseApplication
 import com.codinglance.githubtrendingrepo.R
 import com.codinglance.githubtrendingrepo.adapter.RepoAdapter
 import com.codinglance.githubtrendingrepo.databinding.ActivityMainBinding
 import com.codinglance.githubtrendingrepo.model.Item
 import com.codinglance.githubtrendingrepo.repository.ResultState
 import com.codinglance.githubtrendingrepo.ui.viewModel.RepoViewModel
-import com.facebook.battery.metrics.composite.CompositeMetrics
-import com.facebook.battery.metrics.composite.CompositeMetricsCollector
+import com.facebook.battery.metrics.cpu.CpuFrequencyMetrics
+import com.facebook.battery.metrics.cpu.CpuFrequencyMetricsCollector
 import com.facebook.battery.metrics.cpu.CpuMetrics
 import com.facebook.battery.metrics.cpu.CpuMetricsCollector
+import com.facebook.battery.metrics.devicebattery.DeviceBatteryMetrics
+import com.facebook.battery.metrics.devicebattery.DeviceBatteryMetricsCollector
+import com.facebook.battery.metrics.healthstats.HealthStatsMetrics
+import com.facebook.battery.metrics.healthstats.HealthStatsMetricsCollector
+import com.facebook.battery.metrics.network.NetworkMetrics
+import com.facebook.battery.metrics.network.NetworkMetricsCollector
+import com.facebook.battery.metrics.time.TimeMetrics
+import com.facebook.battery.metrics.time.TimeMetricsCollector
+import com.facebook.battery.reporter.devicebattery.DeviceBatteryMetricsReporter
+
 import dagger.hilt.android.AndroidEntryPoint
+
 
 
 @AndroidEntryPoint
@@ -31,10 +45,37 @@ class MainActivity : AppCompatActivity() {
     val sCollector: CpuMetricsCollector = CpuMetricsCollector()
     private val mInitialMetrics: CpuMetrics = sCollector.createMetrics()
     private val mFinalMetrics: CpuMetrics = sCollector.createMetrics()
+
+    private val timeCollector: TimeMetricsCollector = TimeMetricsCollector()
+    private val timeInitialMetrics: TimeMetrics = timeCollector.createMetrics()
+    private val timeFinalMetrics: TimeMetrics = timeCollector.createMetrics()
+
+    private val cpuFrequencyCollector: CpuFrequencyMetricsCollector = CpuFrequencyMetricsCollector()
+    private val cpuFrequencyInitialMetrics: CpuFrequencyMetrics = cpuFrequencyCollector.createMetrics()
+    private val cpuFrequencyFinalMetrics: CpuFrequencyMetrics = cpuFrequencyCollector.createMetrics()
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    lateinit var healthCollector: HealthStatsMetricsCollector
+    @RequiresApi(Build.VERSION_CODES.N)
+    lateinit var  healthInitialMetrics: HealthStatsMetrics
+    @RequiresApi(Build.VERSION_CODES.N)
+    lateinit var healthFinalMetrics: HealthStatsMetrics
+
+    lateinit var networkCollector: NetworkMetricsCollector
+    lateinit var networkInitialMetrics: NetworkMetrics
+    lateinit var networkFinalMetrics: NetworkMetrics
+
+    lateinit var deviceBatteryCollector: DeviceBatteryMetricsCollector
+    private lateinit var batteryMetricsReporter: DeviceBatteryMetricsReporter
+
+    lateinit var mInitialDeviceMetrics: DeviceBatteryMetrics
+    lateinit var mFinalDeviceMetrics: DeviceBatteryMetrics
+
     private lateinit var  binding: ActivityMainBinding
     private val repoViewModel: RepoViewModel by viewModels()
     private lateinit var mProgressDialog: ProgressDialog
     private lateinit var repoAdapter: RepoAdapter
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val rootView = layoutInflater.inflate(R.layout.activity_main, null, true)
@@ -44,6 +85,21 @@ class MainActivity : AppCompatActivity() {
         binding.viewModel= repoViewModel
         initViewModel()
         binding.searchEt.clearFocus()
+        healthCollector = HealthStatsMetricsCollector(this)
+        healthInitialMetrics =healthCollector.createMetrics()
+        healthFinalMetrics =healthCollector.createMetrics()
+
+        networkCollector = NetworkMetricsCollector(this)
+        networkInitialMetrics = networkCollector.createMetrics()
+        networkFinalMetrics = networkCollector.createMetrics()
+
+        deviceBatteryCollector = DeviceBatteryMetricsCollector(this)
+        batteryMetricsReporter = DeviceBatteryMetricsReporter()
+
+        mInitialDeviceMetrics = deviceBatteryCollector.createMetrics()
+        mFinalDeviceMetrics = deviceBatteryCollector.createMetrics()
+
+
 
         binding.searchEt.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -66,12 +122,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     
+    @RequiresApi(Build.VERSION_CODES.N)
+    @SuppressLint("LongLogTag")
     private fun initViewModel() {
         repoViewModel.repoState.observe(this){
             when(it){
                 ResultState.Loading -> {
                     sCollector.getSnapshot(mInitialMetrics);
-                    Log.d("InitialBatteryMetrics", mInitialMetrics.toString());
+                    //Log.d("InitialBatteryMetrics", mInitialMetrics.toString());
+                    //Log.d("Initial C.P.U. Metrics", mInitialMetrics.toString());
+                    //Log.d("Initial Network Metrics", networkInitialMetrics.toString());
+                    //Log.d("Initial Time Metrics", timeInitialMetrics.toString());
+                    //Log.d("Initial CpuFrequency Metrics", cpuFrequencyInitialMetrics.toString());
+                   // Log.d("Initial Health Metrics", healthInitialMetrics.toString());
+                    Log.d("Initial Battery Metrics", mInitialDeviceMetrics.toString());
+                    Log.d("Initial Battery Metrics", batteryMetricsReporter.toString());
                     displayProgressAnimation()
                 }
                 is ResultState.Success ->{
@@ -80,8 +145,18 @@ class MainActivity : AppCompatActivity() {
                     repoAdapter = RepoAdapter(this,repoViewModel.repolist)
                     binding.repoRecyclerview.adapter = repoAdapter
                     sCollector.getSnapshot(mFinalMetrics);
-                    Log.d("FinalBatteryMetrics", mFinalMetrics.toString());
-                    getBatteryMetrics(mInitialMetrics,mFinalMetrics,"MainActivity")
+//                    Log.d("Final C.P.U. Metrics", mFinalMetrics.toString());
+//                    Log.d("Final Network Metrics", networkFinalMetrics.toString());
+//                    Log.d("Final Time Metrics", timeFinalMetrics.toString());
+//                    Log.d("Final CpuFrequency Metrics", cpuFrequencyFinalMetrics.toString());
+                  //  Log.d("Final Batter Metrics", mFinalDeviceMetrics.toString());
+
+//                    Log.d("C.P.U. Metrics", mFinalMetrics.diff(mInitialMetrics).toString());
+//                    Log.d("Network Metrics", networkFinalMetrics.diff(networkInitialMetrics).toString());
+//                    Log.d("Time Metrics", timeFinalMetrics.diff(timeInitialMetrics).toString());
+//                    Log.d("C.P.U Frequency Metrics", cpuFrequencyFinalMetrics.diff(cpuFrequencyInitialMetrics).toString());
+//                    Log.d("Health Metrics", healthFinalMetrics.diff(healthInitialMetrics).toString());
+                   // Log.d("Battery Metrics", mFinalDeviceMetrics.diff(mInitialDeviceMetrics).toString());
                 }
                 is ResultState.Error ->{
                     Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
@@ -98,8 +173,6 @@ class MainActivity : AppCompatActivity() {
             filterdRepo.addAll((repoViewModel).repolist.filter {
                 it.name.lowercase().contains(text.lowercase()) || it.name.lowercase().contains(text.lowercase())
             }.toList())
-
-
         }
         else{
             filterdRepo.addAll((repoViewModel).repolist)
@@ -125,15 +198,11 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun hideProgressAnimation() {
-
         if(this::mProgressDialog.isInitialized) {
             mProgressDialog.dismiss()
 
         }
     }
 
-    private fun getBatteryMetrics(cpuInitialMetrics:CpuMetrics ,cpuFinalMetrics:CpuMetrics,s:String) {
-        Log.d("$s+Metrics", cpuFinalMetrics.diff(cpuInitialMetrics).toString());
 
-    }
 }
--- app/src/main/java/com/codinglance/githubtrendingrepo/ui/viewModel/RepoViewModel.kt
+++ app/src/main/java/com/codinglance/githubtrendingrepo/ui/viewModel/RepoViewModel.kt
@@ -2,6 +2,11 @@ package com.codinglance.githubtrendingrepo.ui.viewModel
 
 import android.annotation.SuppressLint
 import android.app.Application
+import android.content.Context
+import android.os.BatteryManager
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -12,8 +17,12 @@ import com.codinglance.githubtrendingrepo.repository.Repository
 import com.codinglance.githubtrendingrepo.repository.ResultState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
+import kotlin.system.measureTimeMillis
 
 @HiltViewModel
 class RepoViewModel @Inject constructor(
@@ -23,9 +32,28 @@ class RepoViewModel @Inject constructor(
 {
     var repolist: ArrayList<Item> = ArrayList()
     var mSubscriber= MutableLiveData(false)
+    private val handler = Handler()
+    private lateinit var runnable: Runnable
+    private var startTime: Long = 0
+    private val durationMillis = TimeUnit.MINUTES.toMillis(5)
 
     init {
-    getRepo()
+    startPeriodicApiCalls()
+
+        // Perform the sequence of operations
+        viewModelScope.launch {
+            // Fetch initial battery information
+            getBatteryInfo(application)
+
+            // Start the intensive background computation
+            startHeavyComputation()
+
+            // Delay for 5 minutes
+            delay(5 * 60 * 1000) // 5 minutes in milliseconds
+
+            // Fetch battery information again after 5 minutes
+            getBatteryInfo(application)
+        }
     }
 
 
@@ -36,6 +64,29 @@ class RepoViewModel @Inject constructor(
     val repoState: LiveData<ResultState<RepoResponse>> = _repoState
 
 
+    private fun startPeriodicApiCalls() {
+        startTime = System.currentTimeMillis()
+
+        // Define the Runnable task
+        runnable = object : Runnable {
+            override fun run() {
+                viewModelScope.launch {
+                    // Call your API here
+                    getRepo()
+                }
+                // Check if 5 minutes have passed
+                if (System.currentTimeMillis() - startTime < durationMillis) {
+                    Log.e("viewmodel", "run: task is running ")
+                    // Schedule the next call in 30 seconds (adjust as needed)
+                    handler.postDelayed(this, TimeUnit.SECONDS.toMillis(30))
+                }
+            }
+        }
+
+        // Start the periodic task
+        handler.post(runnable)
+    }
+
     fun getRepo() {
         viewModelScope.launch(Dispatchers.IO)
         {
@@ -45,6 +96,7 @@ class RepoViewModel @Inject constructor(
             try {
 
                 val response=repository.getRepo()
+
                 if (response.items.isNotEmpty()) {
                     _repoState.postValue(ResultState.Success(response))
                     repolist= response.items as ArrayList<Item>
@@ -60,4 +112,56 @@ class RepoViewModel @Inject constructor(
         }
     }
 
+    fun startHeavyComputation() {
+        viewModelScope.launch {
+            val endTime = System.currentTimeMillis() + 5 * 60 * 1000 // 5 minutes from now
+            while (System.currentTimeMillis() < endTime) {
+                performHeavyComputation()
+                // Optionally add a delay if you want to control the frequency of the computation
+                // delay(1000) // Adjust delay to control frequency if needed
+            }
+        }
+    }
+
+    private suspend fun performHeavyComputation() {
+        withContext(Dispatchers.Default) {
+            // Simulate continuous heavy computation
+            while (true) {
+                val timeTaken = measureTimeMillis {
+                    val result = generatePrimes(100000)
+                    println("Computation result size: ${result.size}")
+                }
+                println("Computation time: $timeTaken ms")
+
+                // Break the loop if the total time exceeds 5 minutes
+                val endTime = System.currentTimeMillis() + 5 * 60 * 1000
+                if (System.currentTimeMillis() > endTime) break
+            }
+        }
+    }
+
+    private fun generatePrimes(n: Int): List<Int> {
+        val primes = mutableListOf<Int>()
+        for (i in 2..n) {
+            var isPrime = true
+            for (j in 2 until i) {
+                if (i % j == 0) {
+                    isPrime = false
+                    break
+                }
+            }
+            if (isPrime) primes.add(i)
+        }
+        return primes
+    }
+    fun getBatteryInfo(context: Context) {
+        val batteryManager = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+        val batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        val batteryStatus = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_STATUS)
+        val batteryVoltage = batteryManager.getIntProperty(BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE)
+
+        Log.e("Battery", "Battery Level: $batteryLevel%")
+        Log.e("Battery","Battery Status: $batteryStatus")
+        Log.e("Battery","Battery Voltage: ${batteryVoltage / 1000.0}V")
+    }
 }
